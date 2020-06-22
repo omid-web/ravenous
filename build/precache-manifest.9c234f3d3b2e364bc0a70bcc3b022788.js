275b1e146591a1c9fd2bac5cc75c4993951e9852
Omid Rajabi <omid.345@gmail.com>
2020-06-22 16:48:51 -0400

    added readme and hosted on surge

---
+++ build/precache-manifest.9c234f3d3b2e364bc0a70bcc3b022788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b5833f3a01b830606ef041befc536a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff3147c0d165253e4642",
+    "url": "/static/css/main.f0e63362.chunk.css"
+  },
+  {
+    "revision": "0cfc09a5c79c1cb026a5",
+    "url": "/static/js/2.b58f4f0a.chunk.js"
+  },
+  {
+    "revision": "ff3147c0d165253e4642",
+    "url": "/static/js/main.6769bf23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
